a4ccf60841e85cb2eb4f34d3166edaa5e2f35bf8
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2020-12-26 23:49:46 +0530

    Allow one player to submit and reset tiles at position
    There is no checking if the word is valid at this time.

--- js/scrabble.js
+++ js/scrabble.js
@@ -4,6 +4,10 @@ class Tile {
     this.value = points[letter];
   }
 
+  getLetter() {
+    return this.letter;
+  }
+
   getValue() {
     return points[this.letter];
   }
@@ -12,6 +16,8 @@ class Tile {
 class Square {
   constructor(type = "  ") {
     this.type = type;
+    this.fixed = false;
+    this.tile = null;
   }
 
   setTile(tile) {
@@ -23,7 +29,19 @@ class Square {
   }
 
   getLetter() {
-    if(this.tile) return this.tile.letter;
+    if (this.tile) return this.tile.letter;
+  }
+
+  isEmpty() {
+    return !this.tile;
+  }
+
+  clearTile() {
+    if (this.tile && !this.tile.fixed) this.tile = null;
+  }
+
+  fixTile() {
+    if (this.tile) this.tile.fixed = true;
   }
 
   getPoints() {
@@ -42,13 +60,42 @@ class ScrabbleBoard {
   constructor(boardLayout) {
     this.board = R.map(R.map((type) => new Square(type)))(boardLayout);
   }
+
+  resetTurn() {
+    for (var i = 0; i < 15; i++) {
+      for (var j = 0; j < 15; j++) {
+        this.board[i][j].clearTile();
+      }
+    }
+  }
+
+  finishTurn() {
+    for (var i = 0; i < 15; i++) {
+      for (var j = 0; j < 15; j++) {
+        this.board[i][j].fixTile();
+      }
+    }
+  }
+
+  isEmpty(row, col) {
+    return this.board[row][col].isEmpty()
+  }
+
+  putTile(row, col, tile) {
+    this.board[row][col].setTile(tile)
+  }
 }
 
-const tileBag = R.compose(
+var tileBag = R.compose(
   R.flatten,
   R.map(([letter, count]) => R.times(() => new Tile(letter), count)),
   R.toPairs
 )(lettersDistribution);
+
+var myTiles = [];
+var selectedTile = null;
+var selectedTilePosition = null;
+var myTilesCopy = [];
 
 const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
@@ -57,3 +104,10 @@ const shuffle = (array) => {
   }
   return array;
 };
+
+tileBag = shuffle(tileBag);
+
+//const popFromTileBag = () => {
+//const tile = tileBag.pop();
+//tileBag = shuffle(tileBag)
+//}
--- js/ui.js
+++ js/ui.js
@@ -1,32 +1,95 @@
 scrabbleBoard = new ScrabbleBoard(boardLayout);
-scrabbleBoard.board[0][5].setTile(tileBag.pop());
-scrabbleBoard.board[3][10].setTile(tileBag.pop());
 
 const printBoard = (scrabbleBoard) => {
   var table = R.compose(
     R.join("\n"),
     R.prepend("<table>"),
     R.append("</table>"),
-    R.map(
+    R.addIndex(R.map)((squareRow, row) =>
       R.compose(
         R.join("\n"),
         R.prepend("<tr>"),
         R.append("</tr>"),
-        R.map((square) => {
+        R.addIndex(R.map)((square, col) => {
           const type = square.type.trim();
           const letter = square.getLetter();
-          return `<td class="${letter ? "tile" : R.toLower(type)}">${
-            letter ? letter : type
-          }</td>`;
+          return [
+            `<td`,
+            `class="square ${letter ? "tile" : R.toLower(type)}"`,
+            `data-row=${row}`,
+            `data-col=${col}`,
+            `>`,
+            `${letter ? letter : type == "ST" ? "☆" : type}`,
+            `</td>`,
+          ].join(" ");
         })
-      )
+      )(squareRow)
     )
   )(scrabbleBoard.board);
 
-  console.log(table);
-
-  $(".scrabble-board").append(table);
+  $(".scrabble-board").html(table);
 };
 
-console.log(scrabbleBoard);
 printBoard(scrabbleBoard);
+
+const updateMyTiles = () => {
+  R.times(() => myTiles.push(tileBag.pop()), 7);
+  myTilesCopy = [...myTiles];
+};
+
+const printMyTiles = () => {
+  var tiles = R.compose(
+    R.join("\n"),
+    R.prepend("<table>"),
+    R.append("</table>"),
+    R.addIndex(R.map)(
+      (tile, position) =>
+        `<th class="my-tile" data-position=${position}>${
+          tile ? tile.getLetter() : ""
+        }</th>`
+    )
+  )(myTilesCopy);
+
+  $(".my-tiles").html(tiles);
+};
+
+updateMyTiles();
+printMyTiles();
+
+const resetTurn = () => {
+  myTilesCopy = [...myTiles];
+  printMyTiles();
+  scrabbleBoard.resetTurn();
+  printBoard(scrabbleBoard);
+};
+
+const submit = () => {
+  scrabbleBoard.finishTurn();
+  myTiles = [...myTilesCopy];
+  for (var i = 0; i < 7; i++) {
+    if (!myTiles[i]) myTiles[i] = tileBag.pop();
+  }
+
+  myTilesCopy = [...myTiles];
+  printBoard(scrabbleBoard);
+  printMyTiles();
+};
+
+$(".scrabble-board").on("click", ".square", (event) => {
+  const row = $(event.target).data("row");
+  const col = $(event.target).data("col");
+  if (scrabbleBoard.isEmpty(row, col)) {
+    scrabbleBoard.putTile(row, col, selectedTile);
+    selectedTile = null;
+    myTilesCopy[selectedTilePosition] = null;
+    selectedTilePosition = null;
+    printBoard(scrabbleBoard);
+    printMyTiles();
+  }
+});
+
+$(".my-tiles").on("click", ".my-tile", (event) => {
+  const position = $(event.target).data("position");
+  selectedTile = myTilesCopy[position];
+  selectedTilePosition = position;
+});
